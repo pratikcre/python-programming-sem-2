70d00716aa9ca34e3692c3ae4fb0731868fcd4c6
Pratik mane <pratikmane005@gmail.com>
2026-02-17 11:20:55 +0530

    Add files via upload

---
+++ Exp3/activity1.py
@@ -0,0 +1,15 @@
+# program using nested loops
+"""
+Created on Tue Feb 17 09:46:35 2026
+
+@author: pratik mane
+"""
+#number of receipts
+for receipt in range (1,4):
+    print("Receipt",receipt)
+    
+    #items in each receipt
+    for item in range (1,6):
+        print("item",item)
+        
+    print()
